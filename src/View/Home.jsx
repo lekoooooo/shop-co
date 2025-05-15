6564fe3fce91dee610aef3c4a2ff92084469355d
Levan Didebashvili <levdidebashvili@gmail.com>
2025-05-15 23:36:24 +0400

    feat

--- src/View/Home.jsx
+++ src/View/Home.jsx
@@ -19,7 +19,6 @@ const Home = () => {
       <HappyCustomers />
       <StayUpDated />
       <Footer />
-      <ProductDetail />
     </div>
   );
 };
--- src/components/NewArrivals/NewArrivals.jsx
+++ src/components/NewArrivals/NewArrivals.jsx
@@ -68,3 +68,47 @@ const NewArrivals = () => {
 };
 
 export default NewArrivals;
+
+// import { Link } from "react-router-dom";
+
+// import { Link } from "react-router-dom";
+
+// const ProductCard = ({ product }) => (
+//   <Link
+//     to={`/product/${product.id}`}
+//     className="flex-shrink-0 w-[170px] lg:w-auto"
+//   >
+//     <div className="bg-[#f2f0f1] mb-2 w-full h-[198px] lg:h-[320px]">
+//       <img
+//         src={product.image}
+//         alt={product.title}
+//         className="w-full h-full object-contain"
+//       />
+//     </div>
+//     <h4 className="mb-1 font-medium text-[14px] lg:text-[16px]">
+//       {product.title}
+//     </h4>
+//     <div className="flex items-center gap-1 mb-1">
+//       <img src={`./${product.rating}.svg`} alt="Rating" className="h-4" />
+//       <span className="text-[12px] lg:text-[14px]">
+//         {product.rating}/<span className="text-[#00000099]">5</span>
+//       </span>
+//     </div>
+//     <div className="flex items-center gap-2">
+//       <p className="font-bold text-[20px] lg:text-[22px]">{product.price}</p>
+//       {product.originalPrice && (
+//         <>
+//           <span className="text-[20px] text-gray-400 line-through">
+//             {product.originalPrice}
+//           </span>
+//           <div className="bg-red-100 px-2 py-0.5 rounded-full font-medium text-[12px] text-red-500">
+//             {product.discount}
+//           </div>
+//           <Route path="/product/:productId" element={<ProductDetail />} />
+//         </>
+//       )}
+//     </div>
+//   </Link>
+// );
+
+// export default ProductCard;
