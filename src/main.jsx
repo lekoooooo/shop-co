3c3e7b5425c10e4142eaa1fb9e2d08663f27a94b
Levan Didebashvili <levdidebashvili@gmail.com>
2025-05-19 00:59:29 +0400

    feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./AuthContext/AuthContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </QueryClientProvider>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
