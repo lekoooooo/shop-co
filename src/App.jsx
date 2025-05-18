e996b485a24ae33fcfd24363157ea6a44758273f
Levan Didebashvili <levdidebashvili@gmail.com>
2025-05-19 01:01:16 +0400

    Revert "feature"
    This reverts commit 3c3e7b5425c10e4142eaa1fb9e2d08663f27a94b.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./View/Home";
+import ProductDetail from "./ProductDetail/ProductDetail";
+import Register from "./components/Register/Register";
+import LogIn from "./Login/Login";
+import SignupSuccess from "./components/Register/Success";
+import "./global.css";
+import Casual from "./Casual/Casual";
+import { casualCloth } from "./data/data";
+import "./fonts.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      { index: true, element: <Home /> },
+      { path: "product/:productId", element: <ProductDetail /> },
+      { path: "register", element: <Register /> },
+      { path: "success", element: <SignupSuccess /> },
+      { path: "login", element: <LogIn /> },
+      { path: "casual", element: <Casual casualClothes={casualCloth} /> },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
